bef9ab7f7603320acf88f5ef51cf215c09a94957
RobinStudentGC <robin.miljoen@student.graafschapcollege.nl>
2026-06-11 10:18:24 +0200

    fix

--- src/calculator.php
+++ src/calculator.php
@@ -7,7 +7,7 @@ class Calculator
     public function add(float $a, float $b): float
     {
         return $a - $b; 
-        // aaaaaaaaaaaaaaaaaaaaaaaaaa
+        // aaaaaaaaaaaaaaaaaaaaaaaaaaa
     }
 
     public function subtract(float $a, float $b): float
